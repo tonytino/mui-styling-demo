c64dfee2d1e01de40990cdfa1fc0a3f1ffd11a80
Anthony Hernandez <tony91139@gmail.com>
2019-07-19 13:04:24 -0700

    Document usage of withStyles()

--- src/components/material-ui-theme/withStyles.js
+++ src/components/material-ui-theme/withStyles.js
@@ -6,9 +6,14 @@ import Button from '@material-ui/core/Button';
 
 /*
   withStyles
-  - https://material-ui.com/styles/basics/#higher-order-component-api
+  - https://material-ui.com/styles/advanced/#withstyles
+    - https://material-ui.com/styles/basics/#higher-order-component-api
+  - No access to prop object
+  - Access to theme object for custom styles that depend on the theme object props
+  - Customize nested elements
+    - Generates class names to apply on the root and nested elements
+  - Overrides defined in the custom theme are still applied, if applicable
   - Can be utilized by either a functional or class component
-  - Uncomment the component below to see this example using a class component
 */
 
 // class WithStylesButton extends React.PureComponent {
@@ -25,24 +30,21 @@ import Button from '@material-ui/core/Button';
 function WithStylesButton(props) {
   const { classes } = props;
   return (
-    <Button {...props} className={classes.root}>
-      withStyles
+    <Button {...props} className={classes.myButton}>
+      <span className={classes.mySpan}>
+        withStyles
+      </span>
     </Button>
   );
 }
 
-const styles = {
-  /*
-    NOTE: Intentionally using "root" here, not "button"; "button" causes:
-
-    Warning: Material-UI: the key `button` provided to the classes property is not implemented in ForwardRef(Button).
-    You can only override one of the following: root,label,text,textPrimary,textSecondary,outlined,outlinedPrimary,outlinedSecondary,contained,containedPrimary,containedSecondary,focusVisible,disabled,colorInherit,sizeSmall,sizeLarge,fullWidth.
-
-    https://material-ui.com/api/button/#css
-  */
-  root: {
-    // custom styles go here
+const styles = theme => ({
+  myButton: {
+    '&:hover': {
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+    },
   },
-};
+  mySpan: theme.typography.subtitle1,
+});
 
-export default withStyles(styles)(WithStylesButton);
+export default withStyles(styles, { withTheme: true })(WithStylesButton);
