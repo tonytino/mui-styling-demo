4caa26446650b5ef64a937c78331066c5ccf4bb6
Anthony Hernandez <tony91139@gmail.com>
2019-07-19 11:34:55 -0700

    Document usage of makeStyles()

--- src/components/material-ui-theme/makeStyles.js
+++ src/components/material-ui-theme/makeStyles.js
@@ -7,21 +7,32 @@ import Button from '@material-ui/core/Button';
 /*
   makeStyles
   - https://material-ui.com/styles/advanced/#makestyles
+  - Use this when you need access to the theme or props objects
+    - You can define advanced configurations (e.g. numerous classes can be
+      defined in makeStyles and used on the root and nested elements)
+  - Use this when you need numerous one-off class names to apply on the root
+    and nested elements
+  - Overrides defined in the custom theme are still applied, if applicable
   - Can only be used with functional components (and requires React Hooks)
 */
 
 const useStyles = makeStyles(theme => ({
-  button: {
-    // custom styles go here
+  myButton: {
+    color: props => (
+      props.variant === 'contained' ? null : theme.colors.orange
+    ),
   },
+  mySpan: theme.typography.subtitle1
 }));
 
 function MakeStylesButton(props) {
-  const classes = useStyles();
-
+  // useStyles accepts props, which enables prop-driven customizations
+  const classes = useStyles(props);
   return (
-    <Button {...props} className={classes.button}>
-      makeStyles
+    <Button {...props} className={classes.myButton}>
+      <span className={classes.mySpan}>
+        makeStyles
+      </span>
     </Button>
   );
 };
--- src/components/material-ui-theme/useTheme.js
+++ src/components/material-ui-theme/useTheme.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // https://material-ui.com/customization/default-theme/#material-ui-core-styles-vs-material-ui-styles
+// eslint-disable-next-line
 import { useTheme, withTheme } from '@material-ui/styles';
 // https://material-ui.com/components/buttons/
 import Button from '@material-ui/core/Button';
@@ -12,7 +13,7 @@ import { css, jsx } from '@emotion/core'
   - Use this when you need access to the theme object for custom styles that
     depend on the theme object properties
     - Ideal for one-off designs using theme properties
-    - Overrides defined in the custom theme are still applied, if applicable
+  - Overrides defined in the custom theme are still applied, if applicable
   - Can only be used with functional components
     - Hook Approach:  useTheme()
     - HOC Approach:   withTheme()
